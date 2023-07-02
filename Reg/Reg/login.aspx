acbadaf23628207f0daa3267a1157ae2c8bdf888
Muhammed Anshid OK <anshid283@gmail.com>
2023-07-02 22:25:07 +0530

    first commit

---
+++ Reg/Reg/login.aspx
@@ -0,0 +1,95 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="Reg.login" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+
+<body>
+<style>
+.divv
+{
+    margin: 72px 0 0 30%; 
+}
+.btnDiv
+{
+    margin: 2em 0 0 6em;
+}
+.btn
+{
+    margin: 0 0 0 15px;
+}
+</style>
+
+
+
+
+
+<div class="divv" border="1">
+    <form id="form1" runat="server" border="1">
+    <div>
+        <table style="width: 100%;">
+            <tr>
+                <td>
+                   User Name :
+                </td>
+                <td>
+                    <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+             <tr>
+                <td>
+                   Password  :
+                </td>
+                <td>
+                    <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Mobile :
+                </td>
+                <td>
+                    <asp:TextBox ID="txtMobile" runat="server" TextMode="Number"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Sex:
+                </td>
+                <td>
+                    <asp:DropDownList ID="ddlSex" runat="server">
+                    <asp:ListItem >SELECT</asp:ListItem>
+                    <asp:ListItem Value="M">Male</asp:ListItem>
+                    <asp:ListItem Value="F">Female</asp:ListItem>
+                    <asp:ListItem Value="O">Other</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Date of Birth
+                </td>
+                <td>
+                    <asp:TextBox ID="txtDateOfBirth" runat="server" TextMode="Date"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+        </div>
+        <div class="btnDiv">
+            <asp:Button class="btn" ID="btnSubmit" runat="server" Text="Submit" onclick="btnSubmit_Click" />
+
+            <asp:Button class="btn" ID="btnSearch" runat="server" Text="Search" onclick="btnSearch_Click" />
+
+            <asp:Button class="btn" ID="btnUpdate" runat="server" Text="Update" onclick="btnUpdate_Click" />
+
+            <asp:Button class="btn" ID="btnDelete" runat="server" Text="Delete" onclick="btnDelete_Click" />
+
+        </div>
+    </form>
+</div>
+</body>
+</html>
---
+++ Reg/Reg/login.aspx.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OracleClient;
+using System.Data;
+using System.Configuration;
+//using System.Data.OracleClient.OracleException;
+
+
+
+namespace Reg
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtPassword.Text;
+            string mobile = txtMobile.Text;
+            string sex = ddlSex.Text;
+            string dob = txtDateOfBirth.Text;
+
+            using (OracleConnection con = new OracleConnection(ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString))
+            {
+                try
+                {
+                    con.Open();
+                    string sql = "INSERT INTO customer (USERNAME, PASSWORD, MOBILE, SEX, DOB) VALUES (:username, :password, :mobile, :sex, :dob)";
+                    OracleCommand cmd = new OracleCommand(sql, con);
+                    cmd.Parameters.Add(new OracleParameter("USERNAME", username));
+                    cmd.Parameters.Add(new OracleParameter("PASSWORD", password));
+                    cmd.Parameters.Add(new OracleParameter("MOBILE", mobile));
+                    cmd.Parameters.Add(new OracleParameter("SEX", sex));
+                    cmd.Parameters.Add(new OracleParameter("DOB", dob));
+                    int db = cmd.ExecuteNonQuery();
+
+                    if (db == 1)
+                    {
+                        Response.Write("Record Successfully Inserted: ");
+                    }
+                    else
+                    {
+                        Response.Write("Not Inserted: ");
+
+                    }
+
+
+                    txtUserName.Text = string.Empty;
+                    txtPassword.Text = string.Empty;
+                    txtMobile.Text = string.Empty; 
+                    ddlSex.ClearSelection();
+                    txtDateOfBirth.Text = string.Empty;
+
+                    con.Close();
+                   
+                }
+                catch (Exception ex)
+                {
+                    
+                    Response.Write("Not Connected......!" + ex.Message);
+                }
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtPassword.Text;
+            string mobile = txtMobile.Text;
+            string sex = ddlSex.Text;
+            string dob = txtDateOfBirth.Text;
+
+            using (OracleConnection con = new OracleConnection(ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString))
+            {
+                try
+                {
+                    con.Open();
+                    string sql = "UPDATE customer SET PASSWORD = :password, MOBILE = :mobile, SEX = :sex, DOB = :dob WHERE USERNAME = :username";
+                    OracleCommand cmd = new OracleCommand(sql, con);
+                    cmd.Parameters.Add(new OracleParameter("USERNAME", username));
+                    cmd.Parameters.Add(new OracleParameter("PASSWORD", password));
+                    cmd.Parameters.Add(new OracleParameter("MOBILE", mobile));
+                    cmd.Parameters.Add(new OracleParameter("SEX", sex));
+                    cmd.Parameters.Add(new OracleParameter("DOB", dob));
+                    cmd.ExecuteNonQuery();
+
+                    Response.Write("Record Succesfully Updated: ");
+                    con.Close();
+
+                }
+                catch (Exception ex)
+                {
+
+                    Response.Write("Error......!" + ex.Message);
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            
+            using (OracleConnection con = new OracleConnection(ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString))
+            {
+                try
+                {
+                    con.Open();
+                    string sql = "DELETE FROM customer WHERE USERNAME = (:username)";
+                    OracleCommand cmd = new OracleCommand(sql, con);
+                    cmd.Parameters.Add(new OracleParameter("USERNAME", username));
+                    cmd.ExecuteNonQuery();
+
+                    Response.Write("Record Deleted Inserted: ");
+                    con.Close();
+
+                }
+                catch (Exception ex)
+                {
+
+                    Response.Write("Error......!" + ex.Message);
+                }
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString;
+            string query = "SELECT * FROM customer WHERE USERNAME = :username";
+
+            OracleConnection connection = new OracleConnection(connectionString);
+
+
+            OracleCommand command = new OracleCommand();
+            command.CommandText = query;
+            command.Connection = connection;
+
+            OracleDataAdapter adapter = new OracleDataAdapter();
+            adapter.SelectCommand = command;
+            DataSet dataTable = new DataSet();
+            adapter.Fill(dataTable);
+
+
+            if (dataTable.Tables[0].Rows.Count > 0)
+            {
+                txtPassword.Text = dataTable.Tables[0].Rows[0]["PASSWORD"].ToString();
+                txtMobile.Text = dataTable.Tables[0].Rows[0]["MOBILE"].ToString();
+                ddlSex.SelectedValue = dataTable.Tables[0].Rows[0]["SEX"].ToString();
+                txtDateOfBirth.Text = dataTable.Tables[0].Rows[0]["DOB"].ToString();
+
+            }
+            else
+            {
+                txtUserName.Text = string.Empty;
+                txtPassword.Text = string.Empty;
+                txtMobile.Text = string.Empty;
+                ddlSex.ClearSelection();
+                txtDateOfBirth.Text = string.Empty;
+            }
+        }
+        
+    }
+}
